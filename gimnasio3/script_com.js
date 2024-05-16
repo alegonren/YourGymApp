0f22fbfd0ab7c59f7cb0aafa5ec3fb0362cacc4c
alegonren <138701785+alegonren@users.noreply.github.com>
2024-05-16 14:43:47 +0200

    Add files via upload

--- gimnasio3/script_com.js
+++ gimnasio3/script_com.js
@@ -1,15 +1,15 @@
 const noticiasContainer = document.querySelector('.noticias');
         const noticias = [
-            { titulo: "Noticia 1", imagen: "imagenes/noticia3.jpg", enlace: "https://www.sabervivirtv.com/alimentacion/los-secretos-de-los-japoneses-para-esta-delgados-sin-hacer-dieta_9563" },
-            { titulo: "¿Qué hay que hacer para evitar la flacidez en los brazos?", imagen: "noticia2.jpeg", enlace: "https://elpais.com/escaparate/belleza/2024-04-26/eliminar-flacidez-brazos.html" },
-            { titulo: "Noticia 3", imagen: "imagenes/noticia1.jpg", enlace: "https://www.ejemplo3.com" },
-            { titulo: "Noticia 4", imagen: "imagen4.jpg", enlace: "https://www.ejemplo4.com" },
-            { titulo: "Noticia 5", imagen: "imagen5.jpg", enlace: "https://www.ejemplo5.com" },
-            { titulo: "Noticia 6", imagen: "imagen6.jpg", enlace: "https://www.ejemplo6.com" },
-            { titulo: "Noticia 7", imagen: "imagen7.jpg", enlace: "https://www.ejemplo7.com" },
-            { titulo: "Noticia 8", imagen: "imagen8.jpg", enlace: "https://www.ejemplo8.com" },
-            { titulo: "Noticia 9", imagen: "imagen9.jpg", enlace: "https://www.ejemplo9.com" },
-            { titulo: "Noticia 10", imagen: "imagen10.jpg", enlace: "https://www.ejemplo10.com" }
+            { titulo: "EVITAR PROBLEMAS MÉDICOS AL MONTAR EN BICI", imagen: "imagenes/noticia1.avif", enlace: "https://elpais.com/salud-y-bienestar/enformate/2024-05-16/como-evitar-problemas-fisicos-al-montar-en-bicicleta.html" },
+            { titulo: "DIETA FLEXITARIANA", imagen: "imagenes/noticia2.avif", enlace: "https://elpais.com/salud-y-bienestar/nutrir-con-ciencia/2024-05-14/adoptar-una-dieta-flexitariana-puede-ayudar-al-planeta.html" },
+            { titulo: "LA CULPA DEL DEPORTISTA", imagen: "imagenes/noticia3.avif", enlace: "https://elpais.com/salud-y-bienestar/2024-05-09/la-culpa-del-deportista-cuando-no-entrenar-genera-malestar.html" },
+            { titulo: "CONSECUENCIAS DE PASAR SENTADO 8H AL DIA", imagen: "imagenes/noticia4.avif", enlace: "https://elpais.com/salud-y-bienestar/2024-05-07/coral-sanfeliu-investigadora-del-envejecimiento-estar-sentado-ocho-horas-al-dia-afecta-a-las-conexiones-del-cerebro.html" },
+            { titulo: "COMER INSECTOS Y TRAGAR BULOS", imagen: "imagenes/noticia5.avif", enlace: "https://elpais.com/salud-y-bienestar/nutrir-con-ciencia/2024-04-29/comer-insectos-y-tragar-bulos-mitos-y-leyenda-sobre-la-alimentacion-con-bichos.html" },
+            { titulo: "TOP-5 EJERCICIOS FACILES Y RECOMENDADOS", imagen: "imagenes/noticia6.jpg", enlace: "https://www.sabervivirtv.com/en-forma/ejercicios-faciles-recomendados-por-una-entrenadora-para-sentirte-verte-mas-joven_9561" },
+            { titulo: "¿LAVAR EL ARROZ ANTES DE COCINARLO?", imagen: "imagenes/noticia6.avif", enlace: "https://elpais.com/salud-y-bienestar/nutrir-con-ciencia/2024-02-09/el-riesgo-del-arsenico-en-los-alimentos-conviene-lavar-el-arroz-antes-de-cocinarlo.html" },
+            { titulo: "FOMENTAR LA ACTIVIDAD FISICA EN EMBARAZADAS", imagen: "imagenes/noticia8.avif", enlace: "https://elpais.com/salud-y-bienestar/enformate/2024-01-22/como-fomentar-la-actividad-fisica-en-mujeres-embarazadas.html" },
+            { titulo: "EL MEJOR EJERCICIO PARA EL CORAZON", imagen: "imagenes/noticia9.avif", enlace: "https://elpais.com/salud-y-bienestar/branded/lo-que-importa/2024-04-17/en-el-ejercicio-de-cuidar-el-corazon-la-mujer-va-detras.html" },
+
         ];
         let startIndex = 0;
 
